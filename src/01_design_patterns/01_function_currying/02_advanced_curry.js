28460e7659067705487e0d2fce3682910097203c
Bogdan Nedelcu <bogdanned32@gmail.com>
2023-10-21 16:45:56 +0200

    feat: updated problem 2 description

--- src/01_design_patterns/01_function_currying/02_advanced_curry.js
+++ src/01_design_patterns/01_function_currying/02_advanced_curry.js
@@ -2,11 +2,13 @@
 Write a function named curry that takes a function fn as an argument. 
 The curry function should return a new function that can take multiple arguments 
 either all at once or one at a time. For example:
-    function add(a, b) {
-        return a + b;
+    function add(a, b, c) {
+        return a + b + c;
     }
     const curriedAdd = curry(add);
-    curriedAdd(1, 2); // 3
+    curriedAdd(1, 2)(10); // 13
+    curriedAdd(1)(2)(10); // 13
+    curriedAdd(1, 2, 10); // 13
 */
 
 // HINT: You will need to use recursion to solve this problem!
