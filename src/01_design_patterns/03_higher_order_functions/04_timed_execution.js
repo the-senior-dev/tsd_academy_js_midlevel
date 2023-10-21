8d23932deedbec3d60c3f8c04449191f11199a1c
Bogdan Nedelcu <bogdanned32@gmail.com>
2023-10-21 17:03:40 +0200

    feat: updated problem statement

--- src/01_design_patterns/03_higher_order_functions/04_timed_execution.js
+++ src/01_design_patterns/03_higher_order_functions/04_timed_execution.js
@@ -1,18 +1,21 @@
 /* Higher-Order Functions
 Exercise: Create a Higher-Order Function named timeExecution
-Objective:
 Your task is to create a higher-order function named timeExecution that takes 
 a function as an argument and returns a new function. When the new function is called, 
 it should measure the time taken for the argument function to execute 
 and then return that time along with the result of the function.
 
 Requirements:
-The function timeExecution should accept a function fn as its parameter.
-The returned function should accept any number of arguments.
-The returned function should measure the time before and after the function fn executes.
-The returned function should return an object containing two properties:
-result: The result of the function fn
-time: The time taken for fn to execute in milliseconds
+- the function timeExecution should accept a function fn as its parameter.
+- the returned function should accept any number of arguments.
+- the returned function should measure the time before and after the function fn executes.
+- the returned function should return an object containing two properties: { result: 7, time: 0.1234 }
+
+Example:
+const add = (a, b) => a + b;
+const timedAdd = timeExecution(add);
+const output = timedAdd(3, 4);
+console.log(output); // Output should be something like { result: 7, time: 0.1234 }
 */
 
 function timeExecution(fn) {
@@ -24,15 +27,5 @@ function timeExecution(fn) {
         return {result, time}
     }
 }
-
-// Example
-/*
-const add = (a, b) => a + b;
-const timedAdd = timeExecution(add);
-const output = timedAdd(3, 4); // { result: 7, time: 0.1234 }
-console.log(output);
-*/
-
-// Output should be something like { result: 7, time: 0.1234 }
 
 module.exports = timeExecution;
