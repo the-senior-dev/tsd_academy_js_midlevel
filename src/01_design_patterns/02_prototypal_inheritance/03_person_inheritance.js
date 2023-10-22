fcf088116220750fcf3c7be2caf077844c6d44b0
Bogdan Nedelcu <bogdanned32@gmail.com>
2023-10-22 12:37:05 +0200

    feat: 03 solution

--- src/01_design_patterns/02_prototypal_inheritance/03_person_inheritance.js
+++ src/01_design_patterns/02_prototypal_inheritance/03_person_inheritance.js
@@ -65,49 +65,45 @@ Car.passengers = 4;
 Car.trunkSpace = 400;  // New Property, trunk space in liters
 
 // TODO: Create a ToyotaSupra object that inherits from the Car object
-// const ToyotaSupra = Object.create(Car);
-const ToyotaSupra = {};
+const ToyotaSupra = Object.create(Car);
 
 // TODO: Set the 'name' property of the ToyotaSupra object to "Toyota"
-// ToyotaSupra.name = "Toyota";
+ToyotaSupra.name = "Toyota";
 
 // TODO: Set the 'model' property of the ToyotaSupra object to "Supra"
-// ToyotaSupra.model = "Supra";
+ToyotaSupra.model = "Supra";
 
 // TODO: Set the 'maxSpeed' property of the ToyotaSupra object to 250
-// ToyotaSupra.maxSpeed = 250;
+ToyotaSupra.maxSpeed = 250;
 
 // TODO: Set the 'turbo' property of the ToyotaSupra object to true
-// ToyotaSupra.turbo = true;
+ToyotaSupra.turbo = true;
 
 // TODO: Create a CustomSupra object that inherits from ToyotaSupra
-const CustomSupra = {};
-// const CustomSupra = Object.create(ToyotaSupra);
+const CustomSupra = Object.create(ToyotaSupra);
 
 // TODO: Customize the CustomSupra object
-// For example, you could change its maxSpeed, add custom properties, or even methods.
-// CustomSupra.maxSpeed = 270;  // Increase max speed
-// CustomSupra.customPaint = "Metallic Red";  // Add a custom paint job
+CustomSupra.maxSpeed = 270;  // Increase max speed
+CustomSupra.customPaint = "Metallic Red";  // Add a custom paint job
 
 // TODO: Optionally, add new methods to CustomSupra
-// CustomSupra.showOff = function() {
-//    return "Look at my custom paint and faster speed!";
-// };
+CustomSupra.showOff = function() {
+    return "Look at my custom paint and faster speed!";
+};
 
 // Example 2. MotorBike Object
 // MotorBike Object
-// const MotorBike = Object.create(Vehicle);
-const MotorBike = {}
+const MotorBike = Object.create(Vehicle);
 
-// MotorBike.name = "Honda";
-// MotorBike.model = "CBR 600";
-// MotorBike.wheels = 2;
-// MotorBike.engine = "inline 4";
-// MotorBike.passengers = 2;
-// MotorBike.sidecar = false; // New Property, indicates if the motorbike has a sidecar
-// MotorBike.wheelie = function() {  // New Method
-//     return "Doing a wheelie!";
-// };
+MotorBike.name = "Honda";
+MotorBike.model = "CBR 600";
+MotorBike.wheels = 2;
+MotorBike.engine = "inline 4";
+MotorBike.passengers = 2;
+MotorBike.sidecar = false; // New Property, indicates if the motorbike has a sidecar
+MotorBike.wheelie = function() {  // New Method
+    return "Doing a wheelie!";
+};
 
 // Don't touch this code
 module.exports = {
